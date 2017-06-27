9364036ed89b7d0d0870d3101afbe730c9e760c8
Elliott O'Hara <elliott.ohara@gmail.com>
2017-06-27 09:18:10 -0500

    uplading mods works

--- utils/syncMods.sh
+++ utils/syncMods.sh
@@ -3,5 +3,5 @@ FOLDER_NAME=$1
 UPLOAD_LOCATION=s3://files.pkminecraftcreeper.com/
 
 zip mods.zip $FOLDER_NAME/mods/*.*
-aws s3 cp mods.zip UPLOAD_LOCATION
+aws s3 cp mods.zip $UPLOAD_LOCATION
 rm mods.zip
